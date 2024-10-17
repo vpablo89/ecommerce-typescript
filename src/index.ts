17b722d27e3e62bf94103b763ccd134d66eeeb80
Pablo Vera <verapablodaniel.1989@gmail.com>
2024-10-17 17:21:26 -0300

    stable commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import { Environment } from 'env-types';
 
 import AppFactory from './presentation/factories/AppFactory';
 import DBFactory from './data/factories/DBFactory';
 
-Environment.load()
+
 
 const PORT: string =  process.env.NODE_PORT || '8081'
 
@@ -14,7 +13,7 @@ const PORT: string =  process.env.NODE_PORT || '8081'
 void(async()=>{
     
     const db = DBFactory.create(process.env.DB)
-    console.log(process.env.PORT)
+    
     db.init(process.env.DB_URI || '')
     
     const app = AppFactory.create(process.env.APP_TYPE )   
@@ -26,3 +25,4 @@ void(async()=>{
 })()
 
 
+    
